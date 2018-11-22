9aa40faf1a3eff2804ae4face307960a758015d5
Pierce C Lauring <pclauring@gmail.com>
2018-11-22 11:33:42 -0500

    handle actions with controllers

--- force-app/main/default/aura/campingListItem/campingListItemController.js
+++ force-app/main/default/aura/campingListItem/campingListItemController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
+    packItem : function(component, event, helper) {
+        var item = btnClicked.get("v.item");
+        item.Packed__c = true;
+        component.set("v.item", item);    
+        event.getSource().set("v.disabled", true);
     }
 })
