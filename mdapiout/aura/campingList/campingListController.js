add59a064585e3e07eda71ebb18b6a9141f31662
Pierce C Lauring <pclauring@gmail.com>
2018-11-25 17:21:51 -0500

    connected to apex controller

--- mdapiout/aura/campingList/campingListController.js
+++ mdapiout/aura/campingList/campingListController.js
@@ -1,26 +1,32 @@
 ({
-    clickCreateItem: function (component, event, helper) {
+    // Load expenses from Salesforce
+    doInit: function (component, event, helper) {
+        // Create the action
+        var action = component.get("c.getItems");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.items", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    },
+
+    clickCreateItem : function (component, event, helper) {
         var validitem = component.find('itemform').reduce(function (validSoFar, inputCmp) {
             // Displays error messages for invalid fields
             inputCmp.showHelpMessageIfInvalid();
             return validSoFar && inputCmp.get('v.validity').valid;
         }, true);
         if (validitem) {
-            // Create the new expense
+            // Create the new items
             var newItem = component.get("v.newItem");
-            console.log("Create item: " + JSON.stringify(newItem));
-
-
-            var items = component.get("v.items");
-            items.push(newItem);
-            component.set("v.items", items);
-            component.set("v.newItem", {
-                'sobjectType': 'Camping_Item__c',
-                'Name': '',
-                'Price__c': 0,
-                'Quantity': 0,
-                'Packed__c': false
-            });
+            helper.createItem(newItem, component);
 
         }
     }
