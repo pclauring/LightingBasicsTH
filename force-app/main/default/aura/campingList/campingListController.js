01d2e59560e6ba54a855be5b68f610a3b75f958e
Pierce C Lauring <pclauring@gmail.com>
2018-12-04 22:58:10 -0500

    added missing controller and helper edits to form

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -15,19 +15,5 @@
         });
         // Send action off to be executed
         $A.enqueueAction(action);
-    },
-
-    clickCreateItem : function (component, event, helper) {
-        var validitem = component.find('itemform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-        if (validitem) {
-            // Create the new items
-            var newItem = component.get("v.newItem");
-            helper.createItem(newItem, component);
-
-        }
     }
 })
--- force-app/main/default/aura/campingList/campingListHelper.js
+++ force-app/main/default/aura/campingList/campingListHelper.js
@@ -1,30 +1,7 @@
 ({
-    createItem : function(item, component) {
-        var action = component.get("c.saveItem");
-        action.setParams({
-            "item" : item
-        });
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var items = component.get("v.items");
-                items.push(response.getReturnValue());
-                component.set("v.items", items);
-                component.set("v.newItem", {
-                    'sobjectType': 'Camping_Item__c',
-                    'Name': '',
-                    'Price__c': 0,
-                    'Quantity': 0,
-                    'Packed__c': false
-                });
-            }
-        });
-        $A.enqueueAction(action);
-    },
-
     handleAddItem: function(component, event, helper) {
         var newItem = event.getParam("item");
-        helper.createExpense(component, newItem);
-    },
+        helper.createExpense(component, newItem); //need to create the call back to handle the new item event
+    }
 
 })
